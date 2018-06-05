4a844fb4f0ef40501fc6c36d3c7dee5dde1dfde9
sorayaassadian <sorayaassadian@AMS-GYM-EDV1-15.gym.ams.local>
2018-06-05 15:46:52 +0200

    thjtj

---
+++ GUI/src/db/DBTest.java
@@ -0,0 +1,31 @@
+package db;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		try {
+		new DBTest();
+	} catch (SQLException e) {
+		e.printStackTrace(); 
+		}
+	}
+	
+
+	public DBTest() throws SQLException{
+		//Verbindung zu DB...
+		//pfad
+		Connection conn= DriverManager.getConnection("jdbc:ucanaccess://c:/users/sorayaassadian/dbtest.accdb;memory=false");
+		//Insert ausführen
+		Statement stmt = conn.createStatement();
+		stmt.execute("INSERT INTO address ( vorname, nachname, ort, plz )"
+				+ "VALUES ('soraka', 'käser', 'wien', 1120)");
+		//
+
+	}
+
+}
